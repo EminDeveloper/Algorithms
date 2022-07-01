b9d59faf675871705b98ea6573eaeb67cf0cf28d
Emin <emin.f.mammadov@gmail.com>
2022-07-01 11:02:09 +0400

    BinarySearch

---
+++ src/main/java/nativealgoritm/searching/binarysearch/Main.java
@@ -0,0 +1,11 @@
+package nativealgoritm.searching.binarysearch;
+
+public class Main {
+    public static void main(String args[]){
+
+        int arr[] = {10,20,30,40,50};
+        int key = 40;
+        int last=arr.length-1;
+        BinarySearchExample.binarySearch(arr,0,last,key);
+    }
+}
